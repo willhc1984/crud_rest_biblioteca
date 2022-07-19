3645e3060e565effc003b1568c8774fb05c6ac7d
unknown <willhc1984@gmail.com>
2022-07-19 15:10:51 -0300

    Mapeamento OneToMany - Usuario/Emprestimo

--- src/main/java/com/example/biblioteca/model/Emprestimo.java
+++ src/main/java/com/example/biblioteca/model/Emprestimo.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -23,7 +25,8 @@ public class Emprestimo implements Serializable{
 	private Date dataEmprestimo;
 	private Date dataDevolucao;
 	
-	@Transient
+	@ManyToOne
+	@JoinColumn(name = "usuario_id")
 	private Usuario usuario;
 	
 	@Transient
--- src/main/java/com/example/biblioteca/model/Usuario.java
+++ src/main/java/com/example/biblioteca/model/Usuario.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -25,7 +26,7 @@ public class Usuario implements Serializable{
 	private String telefone;
 	private String email;
 	
-	@Transient
+	@OneToMany(mappedBy = "usuario")
 	private List<Emprestimo> emprestimos = new ArrayList<>();
 	
 	public Usuario() {
