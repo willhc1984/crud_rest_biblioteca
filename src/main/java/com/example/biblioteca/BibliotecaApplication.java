a6ea27ee2212e026346f0f60bef92af90818c537
unknown <willhc1984@gmail.com>
2022-07-19 16:57:03 -0300

    Instanciação de objetos - populando com relacionamento

--- src/main/java/com/example/biblioteca/BibliotecaApplication.java
+++ src/main/java/com/example/biblioteca/BibliotecaApplication.java
@@ -58,31 +58,54 @@ public class BibliotecaApplication implements CommandLineRunner{
 		Emprestimo ep1 = new Emprestimo(null, new Date(), new Date());
 		Emprestimo ep2 = new Emprestimo(null, new Date(), new Date());
 		Emprestimo ep3 = new Emprestimo(null, new Date(), new Date());
-		Emprestimo ep4 = new Emprestimo(null, new Date(), new Date());
+		Emprestimo ep4 = new Emprestimo(null, new Date(), new Date());		
+		ep1.setUsuario(u4);
+		ep2.setUsuario(u4);
+		ep3.setUsuario(u3);
+		ep4.setUsuario(u3);
 		emprestimoRepository.saveAll(Arrays.asList(ep1, ep2, ep3, ep4));
 		
 		Livro l1 = new Livro(null, "Brave New World", 569, 5.15);
 		Livro l2 = new Livro(null, "Harry Potter", 1234, 4.25);
 		Livro l3 = new Livro(null, "Flashbacks", 987, 3.50);
 		Livro l4 = new Livro(null, "1984", 765, 3.10);
+		ep1.setLivro(l4);
+		ep2.setLivro(l3);
+		ep3.setLivro(l2);
+		ep4.setLivro(l1);
 		livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4));
+		emprestimoRepository.saveAll(Arrays.asList(ep1, ep2, ep3, ep4));
 		
 		Genero g1 = new Genero(null, "Ficção");
 		Genero g2 = new Genero(null, "Distopia");
 		Genero g3 = new Genero(null, "Magia");
 		Genero g4 = new Genero(null, "Infantil");
+		l1.setGenero(g4);
+		l2.setGenero(g3);
+		l3.setGenero(g2);
+		l4.setGenero(g1);
 		generoRepository.saveAll(Arrays.asList(g1, g2, g3, g4));
+		livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4));
 		
 		Editora e1 = new Editora(null, "Fantasia");
 		Editora e2 = new Editora(null, "Abril");
 		Editora e3 = new Editora(null, "Globo");
 		Editora e4 = new Editora(null, "Edipro");
+		l1.setEditora(e4);
+		l2.setEditora(e3);
+		l3.setEditora(e2);
+		l4.setEditora(e1);
 		editoraRepository.saveAll(Arrays.asList(e1, e2, e3, e4));
+		livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4));
 		
 		Colecao c1 = new Colecao(null, "Classicos");
 		Colecao c2 = new Colecao(null, "Classicos II");
+		l1.setColecao(c2);
+		l2.setColecao(c2);
+		l3.setColecao(c1);
+		l4.setColecao(c1);		
 		colecaoRepository.saveAll(Arrays.asList(c1, c2));
-		
+		livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4));
 		
 	}
 
