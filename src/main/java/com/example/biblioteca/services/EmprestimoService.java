c2685744d64aec5fc7a7f9c40a29a2065b27aa16
unknown <willhc1984@gmail.com>
2022-07-20 13:02:36 -0300

    Camada Service - EmprestimoService

---
+++ src/main/java/com/example/biblioteca/services/EmprestimoService.java
@@ -0,0 +1,47 @@
+package com.example.biblioteca.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.biblioteca.model.Emprestimo;
+import com.example.biblioteca.repositories.EmprestimoRepository;
+
+@Service
+public class EmprestimoService {
+	
+	@Autowired
+	private EmprestimoRepository repository;
+	
+	public List<Emprestimo> buscarTodos(){
+		List<Emprestimo> emprestimos = repository.findAll();
+		return emprestimos;
+	}
+	
+	public Emprestimo buscarPorId(Integer id) {
+		Optional<Emprestimo> emprestimo = repository.findById(id);
+		return emprestimo.get();
+	}
+	
+	public Emprestimo salvar(Emprestimo emprestimo) {
+		return repository.save(emprestimo);
+	}
+	
+	public void apagar(Integer id) {
+		repository.deleteById(id);
+	}
+	
+	public Emprestimo atualizar(Integer id, Emprestimo emprestimo) {
+		Emprestimo obj = repository.getOne(id);
+		updateData(obj, emprestimo);
+		return repository.save(emprestimo);
+	}
+
+	private void updateData(Emprestimo obj, Emprestimo emprestimo) {
+		obj.setDataDevolucao(emprestimo.getDataDevolucao());
+		obj.setDataEmprestimo(emprestimo.getDataEmprestimo());
+	}
+
+}
--- src/test/java/com/example/biblioteca/BibliotecaApplicationTests.java
+++ src/test/java/com/example/biblioteca/BibliotecaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.campeonato;
+package com.example.biblioteca;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
